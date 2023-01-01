6672b2e6f76055dbd92cb650b6488c92dc31461e
Seonho Kim <sunhowold@naver.com>
2023-01-02 00:59:16 +0900

    로그인 처리하기 - 쿠키 사용

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,46 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(
+            // 쿠키값은 String 이지만 Spring이 Long으로 변환
+            // 로그인 하지 않은 사용자도 해당 컨트롤러로 요청이 들어오기 때문에 'required=false'
+            @CookieValue(name = "memberId", required = false) Long memberId,
+            Model model
+    ) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인
+        // 쿠키가 존재하더라도, 해당 시점에 회원이 탈퇴하거나 여러가지 이유로 회원데이터가 제거될 수 있다.
+        log.info("homeLogin: memberId={}", memberId);
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
